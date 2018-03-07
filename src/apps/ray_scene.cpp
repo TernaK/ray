c327a11792165b6b7b4363806237ba9382312c02
Terna <tkpamber@yahoo.com>
2018-03-06 20:55:08 -0800

    build complex scene

--- src/apps/ray_scene.cpp
+++ src/apps/ray_scene.cpp
@@ -6,28 +6,45 @@ using namespace ray;
 
 int main(int argc, char* argv[]) {
 
-  shared_ptr<Implicit> box = make_shared<Implicit>(ImplicitType::box);
-  shared_ptr<Implicit> sphere = make_shared<Implicit>(ImplicitType::sphere);
+  std::vector<shared_ptr<Implicit>> spheres;
+  for(int i = -1; i <= 1; i++) {
+    for(int j = -1; j <= 1; j++) {
+      if(i == 0 && j == 0) continue;
+      auto sphere = std::make_shared<Implicit>(ImplicitType::sphere);
+      sphere->scale = glm::vec3(1);
+      sphere->position = glm::vec3(j*2.5,i*2.5,1);
+      sphere->material.color = glm::vec3(fabs(j),fabs(i),0.2);
+      spheres.push_back(std::move(sphere));
+    }
+  }
+
+  auto box = std::make_shared<Implicit>(ImplicitType::box);
+  box->material.color = glm::vec3(1,1,0.2);
+  box->rotation = glm::vec3(44,44,0);
+  box->position.z = 1.2;
+  box->scale = glm::vec3(0.5);
+
+  auto plane = std::make_shared<Implicit>(ImplicitType::plane);
+  plane->scale = glm::vec3(6);
+  plane->material.color = glm::vec3(0.2,0.2,0.85);
+  plane->rotation.x = 90;
+  plane->position.z = 0;
 
   shared_ptr<RayScene> scene = make_shared<RayScene>();
   scene->add(box);
-  scene->add(sphere);
+  for(auto& s: spheres)
+    scene->add(s);
+  scene->add(plane);
 
   shared_ptr<Camera> camera = make_shared<Camera>();
   camera->frame_size = glm::vec2(640,480);
-  camera->position = glm::vec3(0,5,5);
-
-  box->material.color = glm::vec3(0.8,0.2,0.2);
-  box->position = glm::vec3(1,0,1);
-  sphere->position = glm::vec3(1,0,0);
+  camera->position = glm::vec3(0,0,10);
   
   RayRenderer renderer;
   renderer.supersample = 2;
-  cv::TickMeter tm;
-  tm.start();
+
   cv::Mat image = renderer.render_scene(scene, camera);
-  tm.stop();
-  cout << "time: " << tm.getTimeSec() << "s" << endl;
+
   cv::imshow("image", image);
   cv::waitKey();
 }
